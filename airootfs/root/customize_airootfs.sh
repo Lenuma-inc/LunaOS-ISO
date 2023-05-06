8ec9ca7f53925bc0ce4fcb8cf3ab7378beddebf0
norz3n <vellynproduction@gmail.com>
2023-05-06 14:13:05 +0300

    Archiso: Added chaotic-aur key enabling

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -1,2 +1,6 @@
-#rm -rf /usr/lib/modules-load.d/nvidia-utils.conf
-rm -rf usr/lib/modprobe.d/nvidia-utils.conf
+#!/usr/bin/env bash
+echo "Enable Chaotic-AUR keys"
+pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+
+pacman-key --lsign-key FBA220DFC880C036
+
