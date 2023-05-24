8e762348984d7f4b2df0f44b8472abce1340151d
norz3n <vellynproduction@gmail.com>
2023-05-24 16:35:24 +0300

    Archiso: Add ALHP key signing

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -4,7 +4,7 @@ pacman-key --init
 
 echo "Populate keyrings"
 
-pacman-key --populate archlinux alhp
+pacman-key --populate archlinux
 
 echo "Enable Chaotic-AUR keys"
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
@@ -13,3 +13,10 @@ pacman-key --lsign-key FBA220DFC880C036
 
 pacman-key --populate chaotic
 
+
+echo "Enable ALHP keys"
+pacman-key --recv-key 2E3B2B05A332A7DB9019797848998B4039BED1CA 
+
+pacman-key --lsign-key 2E3B2B05A332A7DB9019797848998B4039BED1CA
+
+pacman-key --populate alhp
