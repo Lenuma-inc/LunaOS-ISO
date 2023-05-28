a894ef13918df6343f68818fda539454b5637f97
norz3n <vellynproduction@gmail.com>
2023-05-28 10:27:57 +0300

    Archiso: Add fix for pacman.conf file name

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -9,7 +9,7 @@ pacman-key --populate archlinux
 echo "Enable Chaotic-AUR keys"
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 
-pacman-key --lsign-key FBA220DFC880C036
+pacman-key --add /usr/share/pacman/keyrings/chaotic.gpg && pacman-key --lsign-key FBA220DFC880C036
 
 pacman-key --populate chaotic
 
@@ -17,6 +17,10 @@ pacman-key --populate chaotic
 echo "Enable ALHP keys"
 pacman-key --recv-key 2E3B2B05A332A7DB9019797848998B4039BED1CA 
 
-pacman-key --lsign-key 2E3B2B05A332A7DB9019797848998B4039BED1CA
+pacman-key --add /usr/share/pacman/keyrings/alhp.gpg && pacman-key --lsign-key 2E3B2B05A332A7DB9019797848998B4039BED1CA
 
 pacman-key --populate alhp
+
+echo "Fix pacman.conf file name"
+
+mv "/etc/pacman.conf.bak" "/etc/pacman.conf"
