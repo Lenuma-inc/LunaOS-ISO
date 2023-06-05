6a6c93367170ead3604ac7ead67f55898f46896f
Boria138 <boriabloger@protonmail.com>
2023-06-05 12:26:38 +0600

    Add plasma Skell and delete ALHP
    Disable kwallet
    Disable Plasma Welcome
    Set breeze dark theme and two clicks to open file

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -2,25 +2,11 @@
 
 pacman-key --init
 
-echo "Populate keyrings"
-
-pacman-key --populate archlinux
-
 echo "Enable Chaotic-AUR keys"
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 
 pacman-key --add /usr/share/pacman/keyrings/chaotic.gpg && pacman-key --lsign-key FBA220DFC880C036
 
-pacman-key --populate chaotic
 
-
-echo "Enable ALHP keys"
-pacman-key --recv-key 2E3B2B05A332A7DB9019797848998B4039BED1CA 
-
-pacman-key --add /usr/share/pacman/keyrings/alhp.gpg && pacman-key --lsign-key 2E3B2B05A332A7DB9019797848998B4039BED1CA
-
-pacman-key --populate alhp
-
-echo "Fix pacman.conf file name"
-
-mv "/etc/pacman.conf.bak" "/etc/pacman.conf"
+echo "Populate Keys"
+pacman-key --populate
