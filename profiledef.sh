291fdab60b115a20fc06428f3f97c5dcbc340ef6
Boria138 <boriabloger@protonmail.com>
2023-07-28 22:27:29 +0600

    Deleted nvidia proprietary drriver

--- profiledef.sh
+++ profiledef.sh
@@ -8,7 +8,10 @@ iso_application="LunaOS Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+## GRUB
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
+## systemd-boot
+#bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
