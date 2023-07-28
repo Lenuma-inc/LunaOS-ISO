d26efe7fd803c39188c844ebeef6cfc68a4b292c
Boria138 <boriabloger@protonmail.com>
2023-07-28 18:41:27 +0600

    Fix chaotic-aur keys

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -3,9 +3,9 @@
 pacman-key --init
 
 echo "Enable Chaotic-AUR keys"
-pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
 
-pacman-key --add /usr/share/pacman/keyrings/chaotic.gpg && pacman-key --lsign-key FBA220DFC880C036
+pacman-key --lsign-key 3056513887B78AEB
 
 
 echo "Populate Keys"
