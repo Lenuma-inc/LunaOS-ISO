6d39eeff7d5797aab569805fd40c6d65c9fc84dd
Boria138 <boriabloger@protonmail.com>
2023-08-06 14:53:57 +0600

    Huge update ISO

--- profiledef.sh
+++ profiledef.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="lunaos_$(date +%Y%m)"
-iso_label="LunaOS_$(date +%Y%m)"
+iso_name="lunaos"
+iso_label="LunaOS"
 iso_publisher="Lenuma Team"
 iso_application="LunaOS Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
@@ -17,11 +17,10 @@ pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'zstd' '-Xcompression-level' '3' '-b' '1M')
 file_permissions=(
-    ["/etc/shadow"]="0:0:400"
-    ["/root"]="0:0:750"
-    ["/root/.automated_script.sh"]="0:0:755"
-    ["/usr/local/bin/choose-mirror"]="0:0:755"
-    ["/usr/local/bin/Installation_guide"]="0:0:755"
-    ["/usr/local/bin/livecd-sound"]="0:0:755"
-    ["/etc/gshadow"]="0:0:0400"
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/bin/choose-mirror"]="0:0:755"
 )
