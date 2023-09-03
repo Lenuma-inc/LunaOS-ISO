de86bcfa2c066f9f1c54e63f0aefff1d39174d96
Boria138 <boriabloger@protonmail.com>
2023-09-03 13:51:43 +0600

    Deleted reflector

--- profiledef.sh
+++ profiledef.sh
@@ -22,5 +22,4 @@ file_permissions=(
   ["/etc/polkit-1/rules.d"]="0:0:750"
   ["/etc/sudoers.d"]="0:0:750"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
 )
