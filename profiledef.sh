fd23c3666322951ea6de2dce6dcfeee24add3e33
Boris Yumankulov <boria138@altlinux.org>
2025-06-09 10:37:58 +0500

    Sync with archiso relang config
    Signed-off-by: Boris Yumankulov <boria138@altlinux.org>

--- profiledef.sh
+++ profiledef.sh
@@ -8,14 +8,12 @@ iso_application="LunaOS Live/Rescue DVD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-## GRUB
-#bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
-## systemd-boot
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
